4fcc93c84db5dcd17c8b80acaf12e8563e4630b0
unknown <isaiasodr@gmail.com>
2022-06-02 19:04:03 -0300

    create of logic of inventory sold out

--- controllers/SalesController.js
+++ controllers/SalesController.js
@@ -4,10 +4,10 @@ const Products = require('../models/Products')
 
 module.exports = class SalesController {
 
-    static async productsSales(req,res){
+    static async productsSales(req, res) {
         const id = req.params.id
-        const products = await Products.findOne({where:{id:id},raw:true})
-        res.render('sales/salesadd',{products})
+        const products = await Products.findOne({ where: { id: id }, raw: true })
+        res.render('sales/salesadd', { products })
     }
     static salesAdd(req, res) {
         res.render('sales/salesadd')
@@ -15,22 +15,60 @@ module.exports = class SalesController {
 
     static async salesAddSave(req, res) {
         const sales = {
-            description:req.body.description,
+            amountproducts: req.body.amountproducts,
+            description: req.body.description,
             amount: req.body.amount,
             price: req.body.price,
-            total: req.body.amount*req.body.price,
+            total: req.body.amount * req.body.price,
             productId: req.body.product
         }
+        if (sales.amount > sales.amountproducts) {
+            req.flash('message', 'estoque indisponível')
+            res.render('sales/salesadd')
+            return
+        } 
+
+        await Sales.create(sales)
+        
+
+        res.redirect('/sales')
+    }
+
+    static async AllSales(req, res) {
+        const sales = await Sales.findAll({ raw: true })
+        res.render("sales/allsales", { sales })
+    }
+    static async updatesales(req, res) {
+        const id = req.params.id
+        const sales = await Sales.findOne({ where: { id: id }, raw: true })
+        res.render("sales/updatesales", { sales })
+    }
+    static async UpdateSalesSave(req, res) {
+
+        const id = req.body.id
+        const sales = {
+
+            description: req.body.description,
+            amount: req.body.amount,
+            price: req.body.price,
+            total: req.body.amount * req.body.price,
+            productId: req.body.productId
+        }
+
         try {
-            await Sales.create(sales)
+            await Sales.update(sales, { where: { id: id } })
         } catch (error) {
             console.log(error)
         }
-        
-
-        res.redirect('/salesadd')
+        res.redirect("/sales")
     }
 
-    
+    static async deletesales(req, res) {
+        const id = req.body.id
+        await Sales.destroy({ where: { id: id } })
+        res.redirect('/sales')
+    }
+
+
 
 }
--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -5,6 +5,10 @@ const SalesController = require('../controllers/SalesController')
 router.get('/salesadd/:id',SalesController.productsSales)
 router.get('/salesadd',SalesController.salesAdd)
 router.post('/salesadd',SalesController.salesAddSave)
+router.get('/sales',SalesController.AllSales)
+router.get('/salesupdate/:id',SalesController.updatesales)
+router.post('/salesupdate',SalesController.UpdateSalesSave)
+router.post('/delete',SalesController.deletesales)
 
 
 
